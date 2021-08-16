48847e1ff3dca664c6a40b5974fbfb8bed5be51d
alvinbank1 <61926776+alvinbank1@users.noreply.github.com>
2021-08-16 18:42:21 +0900

    Create script2.js

---
+++ fake-windows-update/script2.js
@@ -0,0 +1,15 @@
+var count = 0;
+var counter = setInterval(timer, 14830);
+
+function timer() {
+    count = count + 1;
+    if (count <= 0) {
+        clearInterval(counter);
+        return;
+    }
+    document.getElementById("timer").innerHTML = count + '%';
+    if (count > 99) {
+        count = 0;
+        return;
+    }
+}
