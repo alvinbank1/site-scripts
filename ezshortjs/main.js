e159d5d2da505a853fd522de9ca0c5d47d0ea6ab
alvinbank1 <61926776+alvinbank1@users.noreply.github.com>
2021-08-17 18:02:41 +0900

    Update main.js

--- ezshortjs/main.js
+++ ezshortjs/main.js
@@ -1,1 +1,1 @@
-function ar(message) { alert(message) } function conlog(message) { console.log(message) }
+function ar(message) { alert(message) } function conlog(message) { console.log(message) } function cse(name) { name.close } function wprt() { window.print()}
